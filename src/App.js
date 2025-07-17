73c6df94c85af108e77f4ea70d2cc4bd9ba87c21
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-16 21:54:18 -0700

    added file cdoe App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChatPage from './pages/ChatPage/ChatPage';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app-container">
+      <ChatPage />
+    </div>
+  );
+}
+
+export default App;
