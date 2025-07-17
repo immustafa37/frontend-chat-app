3cdd7ef641a4773ce78d13bb825250fbaccc4c5e
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-16 21:50:01 -0700

    added file code ChatPage.jsx

---
+++ src/pages/ChatPage/ChatPage.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import ChatHeader from '../../components/ChatHeader/ChatHeader';
+import MessageBubble from '../../components/MessageBubble/MessageBubble';
+import MessageForm from '../../components/MessageForm/MessageForm';
+import './ChatPage.scss';
+
+const ChatPage = () => {
+  const [messages, setMessages] = useState([
+    { id: 1, text: 'Hey there! 👋', avatar: 'https://i.pravatar.cc/150?img=5', isCurrentUser: false },
+    { id: 2, text: 'How are you doing today?', avatar: 'https://i.pravatar.cc/150?img=5', isCurrentUser: false },
+    { id: 3, text: "I'm good, thanks! How about you?", avatar: 'https://i.pravatar.cc/150?img=1', isCurrentUser: true },
+  ]);
+
+  const user = {
+    name: 'Sarah Johnson',
+    status: 'Online',
+    avatar: 'https://i.pravatar.cc/150?img=5'
+  };
+
+  const handleSendMessage = (message) => {
+    const newMessage = {
+      id: messages.length + 1,
+      text: message,
+      avatar: 'https://i.pravatar.cc/150?img=1',
+      isCurrentUser: true
+    };
+    setMessages([...messages, newMessage]);
+  };
+
+  return (
+    <div className="chat-app">
+      <ChatHeader user={user} />
+      <div className="chat-messages">
+        {messages.map((message) => (
+          <MessageBubble 
+            key={message.id} 
+            message={message} 
+            isCurrentUser={message.isCurrentUser} 
+          />
+        ))}
+      </div>
+      <MessageForm onSend={handleSendMessage} />
+    </div>
+  );
+};
+
+export default ChatPage;
